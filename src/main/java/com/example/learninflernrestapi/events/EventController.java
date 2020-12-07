10d2355b0ab6d537ed3d2cacc865d84aadd77b22
김경록 <goodboy3421@gmail.com>
2020-12-08 01:46:53 +0900

    refactor: EventSource refactor because unpdate version

--- src/main/java/com/example/learninflernrestapi/events/EventController.java
+++ src/main/java/com/example/learninflernrestapi/events/EventController.java
@@ -2,6 +2,8 @@ package com.example.learninflernrestapi.events;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
@@ -36,15 +40,19 @@ public class EventController {
             return ResponseEntity.badRequest().body(errors);
         }
 
-        Event event = modelMapper.map(eventDto, Event.class);
-        event.update();
-        Event newEvent = eventRepository.save(event);
+        Event newEvent = eventRepository.save(modelMapper.map(eventDto, Event.class));
+        Integer eventId = newEvent.getId();
+        newEvent.update();
 
-        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(eventId);
         URI createdUri = selfLinkBuilder.toUri();//uri를 만들 때는 HATEOS(=헤이티오스)에서 제공하는 linkTo(), methodOn() 메소드 사용 하면 편리하게 만들 수 있다.
-        EventResource eventResource = new EventResource(event); // todo: 생성자로 생성하는 방식은 deprecated되어 있는데, 이를 어떻게 개선할 수 있을까?
-        eventResource.add(linkTo(EventController.class).withRel("query-events"));
-        eventResource.add(selfLinkBuilder.withRel("update-event")); // update를 하는 것도, self랑 링크는 같은데, relation과 메서드만 다르다
+        List<Link> links = Arrays.asList(
+                selfLinkBuilder.slash(eventId).withSelfRel(),
+                selfLinkBuilder.withRel("query-events"),
+                selfLinkBuilder.withRel("update-event") // update를 하는 것도, self랑 링크는 같은데, relation과 메서드만 다르다
+        );
+
+        EntityModel<Event> eventResource = EntityModel.of(newEvent, links); // new 생성자로 EntityModel 객체 생성하는 방식 deprecated되었고, of 팩토리 메서드 사용 권장!
         return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
